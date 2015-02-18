f726aea3823e9428d8d27f7e33061ea88d647246
wolfgangleon <wolfgangrleonh@gmail.com>
2015-02-18 00:21:35 -0500

    fadeout front page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 $(document).ready( function() {
+
+	/* --- Variables ----*/
 	
 
-});
+	/* ---- Front Page hiding --- */ 
+
+	$('#front-page').click( function() {
+		$(this).fadeOut('slow');
+		$('body').css('background','url("")');
+		$('#quizz-page').show();
+		$('header').append('<h1 class="quotes">hola</h1>')
+		$('header h1').html($('.quotes').html());
+	});
+
+
+
+});  // End of Document Script
