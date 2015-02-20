0e3758f9cea9e21d5bd419cd0b0c66b502f59518
wolfgangleon <wolfgangrleonh@gmail.com>
2015-02-19 19:21:13 -0500

    Quiz misspelling

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(document).ready( function() {
 	$('#front-page').click( function() {
 		$(this).fadeOut('slow');
 		$('body').css('background','url("")');
-		$('#quizz-page').show();
+		$('#quiz-page').show();
 		$('header').append('<h1 class="quotes"></h1>')
 		$('header h1').html($('.quotes').html());
 	});
